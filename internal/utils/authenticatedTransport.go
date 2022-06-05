f61b38f2cceae5de25fd4aa8ca321f60487fa04f
Dov Alperin <git@dov.dev>
2022-06-05 11:59:39 -0400

    Add an API debug mode
    Closes #16

--- internal/utils/authenticatedTransport.go
+++ internal/utils/authenticatedTransport.go
@@ -9,9 +9,13 @@ type Transport struct {
 	UnderlyingTransport http.RoundTripper
 	Token               string
 	Ctx                 context.Context
+	EnableDebugTrace    bool
 }
 
 func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Add("Authorization", "Bearer "+t.Token)
+	if t.EnableDebugTrace {
+		req.Header.Add("Fly-Force-Trace", "true")
+	}
 	return t.UnderlyingTransport.RoundTrip(req)
 }
